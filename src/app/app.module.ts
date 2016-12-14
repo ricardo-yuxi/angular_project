6b03bf010d0cfadb8c2cc1ba0711e8df323cd929
Ricardo Rojas <reddrum7@gmail.com>
2016-12-13 22:48:47 -0500

    include RouterModule, refactor NavComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
+    // Root features
     AppComponent,
-    CoursesComponent
+    NavComponent,
+    // Course feature
+    CoursesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses',
+        component: CoursesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
